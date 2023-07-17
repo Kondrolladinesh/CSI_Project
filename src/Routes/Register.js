493cf43ae23390e297c5b58369bc6fce150524f5
Kondrolla Dinesh Reddy <77775104+Kondrolladinesh@users.noreply.github.com>
2023-07-17 16:21:42 +0530

    Update Register.js

--- src/Routes/Register.js
+++ src/Routes/Register.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, Navigate } from 'react';
+import React, {useState, useEffect } from 'react';
 import "./Routes.css";
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 const Register = () => {
     const [name, setName] = useState("");
