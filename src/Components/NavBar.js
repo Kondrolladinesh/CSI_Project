479e15aa1c1e8c536f0253eb189726ac6f35fc45
dinesh <dineshreddykondrolla@gmail.com>
2023-07-18 12:29:18 +0530

    refresh issue commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import './NavBar.css';
-import companyLogo from '../Images/CompanyLogo1.png'
+import companyLogo from '../Images/CompanyLogo1.png';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -13,9 +14,9 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="nav-header">
-        <a href="/home" className="nav-logo">
+        <NavLink href="/home" className="nav-logo">
           <img src={companyLogo} alt='company-logo'/>
-        </a>
+        </NavLink>
         <div className={`nav-toggle ${isMenuOpen ? 'active' : ''}`} onClick={handleMenuClick}>
           <FaBars />
         </div>
@@ -23,16 +24,14 @@ const NavBar = () => {
       <div className='nav-item-list'>
         <ul className={`navbar-nav ${isMenuOpen ? 'active' : ''}`}>
             <li className="nav-item">
-                <a href="/home" className="nav-link">Home</a>
+                <NavLink to="/home" className="nav-link">Home</NavLink>
             </li>
             <li className="nav-item">
-                <a href="/viewblog" className="nav-link">View Blogs</a>
+                <NavLink to="/viewblog" className="nav-link">View Blogs</NavLink>
             </li>
-            {/* <li className="nav-item">
-                <a href="/contact" className="nav-link">Contact</a>
-            </li> */}
+
             <li className="nav-item">
-                <a href="/" className="nav-link" style={{color:'red'}} onClick={()=>localStorage.setItem("authToken",'')}>Logout</a>
+                <NavLink to="/" className="nav-link" onClick={()=>localStorage.setItem("authToken",'')}>Logout</NavLink>
             </li>
         </ul>
       </div>
